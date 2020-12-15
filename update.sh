b449c6259a14e502c6872f331e0a68293d98e332
Simon Christensen <c3lphie@protonmail.com>
2020-12-15 12:34:08 +0100

    minor fix

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 set -euo pipefail
 
 git pull
-sudo cp api.php /var/www/html/api.php
-sudo cp database.php /var/www/html/database.php
-sudo cp index.php /var/www/html/index.php
+sudo cp Api.php /var/www/html/api.php
+sudo cp Database.php /var/www/html/database.php
+sudo cp Index.php /var/www/html/index.php
