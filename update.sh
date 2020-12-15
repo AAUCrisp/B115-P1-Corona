4437bfc1e1cc735241810c5a3386e5cb115118c2
Simon Christensen <c3lphie@protonmail.com>
2020-12-15 11:10:57 +0100

    Minor edit

--- update.sh
+++ update.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 git pull
-sudo ln -s -f Api.php /var/www/html/Api.php
-sudo ln -s -f Database.php /var/www/html/Database.php
-sudo ln -s -f Index.php /var/www/html/index.php
+sudo cp Api.php /var/www/html/Api.php
+sudo cp Database.php /var/www/html/Database.php
+sudo cp Index.php /var/www/html/index.php
 
